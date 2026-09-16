0bb5cb454c28c5e2cb86836568f996d93cbb1115
teoscrvn-glitch <teoscrvn@gmail.com>
2026-09-17 00:52:10 +0700

    Update API_BASE_URL in config.js

--- frontend/assets/js/config.js
+++ frontend/assets/js/config.js
@@ -1,4 +1,4 @@
 window.TEO_CONFIG={
-  API_BASE_URL:'',
+  API_BASE_URL:'https://lai-hup-file-teo-vip.teoscrvn.workers.dev',
   GOOGLE_CLIENT_ID:'YOUR_GOOGLE_CLIENT_ID.apps.googleusercontent.com'
 };
